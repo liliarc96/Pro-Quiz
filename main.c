f361404e79eace42ad6494529ad7f826ac6bb9ea
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-05-07 20:51:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,3 +2,41 @@
 //Headers:
 #include "ranking.h"
 #include "menu.h"
+
+int main(void) {
+  int opcao, repeticao;
+  menu();
+  scanf("%d", &opcao);
+  while(repeticao == 1){
+    switch(opcao){
+      case 1:
+        printf("\n----COMEÇAR JOGO----\n\nDigite um número: ");
+        scanf("%d", &opcao);
+        break;
+      case 2:
+        printf("\n----VER RANKING----\n\nDigite um número: ");
+        scanf("%d", &numero);
+        menu();
+        scanf("%d", &opcao);
+        break;
+      case 3:
+        printf("\n----CADASTRAR PERGUNTAS----\n\n");
+        menu();
+        scanf("%d", &opcao);
+        break;
+      case 4:
+        printf("\n----CRÉDITOS----\n\n");
+        menu();
+        scanf("%d", &opcao);
+        break;
+      case 0:
+        printf("\nAdeus!");
+        repeticao = 0;
+        break;
+      default:
+        printf("Opção desejada é inexistente, tente novamente: ");
+        menu();
+        scanf("%d", &opcao);
+        break;
+    }
+  } 
