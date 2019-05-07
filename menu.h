394cf91f3f2c134676fa3ab84a1f96ba2b266304
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-05-07 20:45:14 -0300

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,9 @@
+void menu(){
+  printf("\n    ----MENU----\n\n");
+  printf("1: Jogar;\n\n");
+  printf("2: Ver Ranking;\n\n");
+  printf("3: Cadastrar Perguntas;\n\n");
+  printf("4: Creditos;\n\n");
+  printf("0: Sair;\n\n\n");
+  printf("Digite uma opção do menu: ");
+}
