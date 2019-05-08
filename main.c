2f08cbd877d0d68b7e7251797328425784c4e7a9
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-05-07 21:04:15 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,18 +4,22 @@
 #include "menu.h"
 
 int main(void) {
+  //Variáveis de controle:
   int opcao, repeticao;
+  
+  //Chama o menu:
   menu();
+  
   scanf("%d", &opcao);
   while(repeticao == 1){
     switch(opcao){
       case 1:
         printf("\n----COMEÇAR JOGO----\n\nDigite um número: ");
+        menu();
         scanf("%d", &opcao);
         break;
       case 2:
         printf("\n----VER RANKING----\n\nDigite um número: ");
-        scanf("%d", &numero);
         menu();
         scanf("%d", &opcao);
         break;
@@ -39,4 +43,6 @@ int main(void) {
         scanf("%d", &opcao);
         break;
     }
+    
+    return 0;
   } 
