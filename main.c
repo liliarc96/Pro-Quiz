0fddc875eed7b0b138f500f8ac39ff826e82c5bb
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-05-09 16:52:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,105 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include "manipular_arquivo.h"
-#include "menu.h"
+#ifndef MANIPULAR_ARQUIVO_H_INCLUDED
+#define MANIPULAR_ARQUIVO_H_INCLUDED
 
-int main()
-{
-  //Muda para o idioma do sistema:
-  setlocale(LC_ALL,"Portuguese");
+/*
+Para abrir tanto o ranking.txt para modificar o ranking quanto
+abrir o perguntas(dificuldade) para adicionar novas perguntas
+*/
 
-  //Variáveis de controle:
-  int opcao, repeticao = 1;
-  char dificuldade;
-  char * nomeArquivo;
+//Função que recebe o nome do arquivo (3 opções):
+int abrirArquivo(char * nArquivo){
+    FILE * arquivo;
+    char caractere;
 
-  //Intro,aparece o título e após isso a tela limpa
-  titulo();
-  system("pause");
-  system("cls");
-  //Chama o menu:
-  menu();
+    //Abre arquivo:
+    arquivo = fopen(nArquivo, "r");
+    if(arquivo == NULL){
+       printf("\nO arquivo não pode ser aberto...");
+       exit(EXIT_FAILURE);
+    }else{
+        //TESTE MOSTRAR CONTEÚDO DO ARQUIVO NO CONSOLE
+        do{
+            //Lê caractere do arquivo:
+            caractere = fgetc(arquivo);
 
-  scanf("%d", &opcao);
-  system("cls");
+            if(caractere == EOF){
+               break;//Se for final da linha, break (se o EOF for impresso aparece ÿ)
+            }
+            //Mostra caractere no console:
+            putchar(caractere);
 
-  while(repeticao == 1){
-    switch(opcao){
-      case 1:
-        printf("\n----COMEÇAR JOGO----\n\n");
+        } while(caractere != EOF);
+
         /*
-        Alguma coisa aparece na tela...
+        EOF significa End Of Line /!\
         */
-        system("pause");
-        menu();
-        scanf("%d", &opcao);
-        system("cls");
-        break;
-      case 2:
-        printf("\n----VER RANKING----\n\n");
-
-        nomeArquivo = "ranking.txt";
-        abrirRanking(nomeArquivo);
-        printf("\n");
-
-        system("pause");
-        menu();
-        scanf("%d", &opcao);
-        system("cls");
-        break;
-      case 3:
-        printf("\n----CADASTRAR PERGUNTAS----\n\n");
-        printf("Deseja cadastrar uma pergunta de qual dificuldade?\n"
-               "\nPressione E para fáceis, M para intermediárias e H para difíceis\n");
-
-        fflush(stdin);
-        dificuldade = getchar();
-
-        if(dificuldade == 'E'){
-            printf("\nEscolheu perguntas fáceis.\n\n");
-            nomeArquivo = "perguntasfaceis.txt";
-            abrirArquivo(nomeArquivo);
-        }else if(dificuldade == 'M'){
-            printf("\nEscolheu perguntas intermediárias.\n\n");
-            nomeArquivo = "perguntasintermediarias.txt";
-            abrirArquivo(nomeArquivo);
-        }else if(dificuldade == 'H'){
-            printf("\nEscolheu perguntas difíceis.\n\n");
-            nomeArquivo = "perguntasdificeis.txt";
-            abrirArquivo(nomeArquivo);
-        }else{
-            printf("\nOpção incorreta.");
-        }
-
-        printf("\n");
-        system("pause");
-        menu();
-        scanf("%d", &opcao);
-        system("cls");
-        break;
-      case 4:
-        printf("\n----CRÉDITOS----\n\n");
-        /*
-        Algo aparece...
-        */
-        system("pause");
-        menu();
-        scanf("%d", &opcao);
-        system("cls");
-        break;
-      case 0:
-        system("cls");
-        printf("\nAdeus!");
-        repeticao = 0;
-        break;
-      default:
-        printf("Opção desejada é inexistente, tente novamente: ");
-        menu();
-        scanf("%d", &opcao);
-        system("cls");
-        break;
     }
-  }
+
+    //Fechar arquivo:
+    fclose(arquivo);
     return 0;
 }
+
+int abrirRanking(){
+    FILE * arquivo;
+    char caractere;
+
+
+    //Abre arquivo:
+    arquivo = fopen("pontuacao.txt", "r");
+    if(arquivo == NULL){
+       printf("\nO arquivo não pode ser aberto...");
+       exit(EXIT_FAILURE);
+    }else{
+        do{
+            caractere = fgetc(arquivo);
+            if(caractere == EOF){
+               break;
+            }
+            putchar(caractere);
+
+        } while(caractere != EOF);
+    }
+
+    //Fechar arquivo:
+    fclose(arquivo);
+    return 0;
+}
+#endif // MANIPULAR_ARQUIVO_H_INCLUDED
