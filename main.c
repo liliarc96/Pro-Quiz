ac367707b96590b23290388f8142742e72232324
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-05-09 14:07:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-//Headers:
 #include "manipular_arquivo.h"
 #include "menu.h"
 
-int main() {
-  
+int main()
+{
   //Muda para o idioma do sistema:
   setlocale(LC_ALL,"Portuguese");
-  
+
   //Variáveis de controle:
   int opcao, repeticao = 1;
   char opcaoMenu;
   char * nomeArquivo;
-  
+
+  //Intro,aparece o título e após isso a tela limpa
+  titulo();
+  system("pause");
+  system("cls");
   //Chama o menu:
   menu();
-  
+
   scanf("%d", &opcao);
+  system("cls");
+
   while(repeticao == 1){
     switch(opcao){
       case 1:
         printf("\n----COMEÇAR JOGO----\n\n");
+        /*
+        Alguma coisa aparece na tela...
+        */
+        system("pause");
         menu();
         scanf("%d", &opcao);
+        system("cls");
         break;
       case 2:
         printf("\n----VER RANKING----\n\n");
-        
+
         nomeArquivo = "ranking.txt";
         abrirArquivo(nomeArquivo);
-        
+        system("pause");
         menu();
         scanf("%d", &opcao);
+        system("cls");
         break;
       case 3:
         printf("\n----CADASTRAR PERGUNTAS----\n\n");
-        printf("Deseja cadastrar uma pergunta de qual dificuldade?\n"
-               "Pressione E para fáceis, M para intermediárias e H para difíceis\n");
-        
+        printf("Deseja cadastrar uma pergunta de qual dificuldade?"
+               "\nPressione E para fáceis, M para intermediárias e H para difíceis\n");
+
         fflush(stdin);
         opcaoMenu = getchar();
-        
+
         if(opcaoMenu == 'E'){
             printf("\nEscolheu perguntas fáceis.");
             nomeArquivo = "perguntasfaceis.txt";
@@ -58,16 +69,23 @@ int main() {
         }else{
             printf("\nOpção incorreta.");
         }
-        
+        system("pause");
         menu();
         scanf("%d", &opcao);
+        system("cls");
         break;
       case 4:
         printf("\n----CRÉDITOS----\n\n");
+        /*
+        Algo aparece...
+        */
+        system("pause");
         menu();
         scanf("%d", &opcao);
+        system("cls");
         break;
       case 0:
+        system("cls");
         printf("\nAdeus!");
         repeticao = 0;
         break;
@@ -75,8 +93,9 @@ int main() {
         printf("Opção desejada é inexistente, tente novamente: ");
         menu();
         scanf("%d", &opcao);
+        system("cls");
         break;
     }
-    
+  }
     return 0;
-  } 
+}
