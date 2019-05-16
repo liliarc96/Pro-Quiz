cc23829a6c5b3ae9daa829ee18f9f6bd6dc938b1
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-05-16 16:56:32 -0300

    Update perguntas.h

--- Headers/perguntas.h
+++ Headers/perguntas.h
@@ -64,5 +64,94 @@ int startquiz(char * nArquivo){
     fclose(arquivo);
     return 0;
 }
+//TESTE
+int testeArray(){
+    int i=0;
+    int numperguntas = 10;
+    char* perguntas[10];
+    char linhas[21];
+
+    FILE * arquivo;
+    arquivo = fopen("perguntasfaceis.txt", "r");
+
+    while(fgets(linhas, sizeof linhas, arquivo)!=NULL) {
+
+    printf("%s", linhas);
+    perguntas[i]= malloc(sizeof(linhas));
+    strcpy(perguntas[i],linhas);
+    i++;
+    numperguntas++;
+  }
+
+  for (int j=0 ; j<numperguntas+1; j++) {
+  printf("\n%s", perguntas[j]);
+ }
+
+ fclose(arquivo);
+ return 0;
+}
+
+//TESTE
+int testeArray2(){
+    static const char filename[] = "perguntasfaceis.txt";
+    FILE *file = fopen ( filename, "r" );
+    int i, j;
+    char arra[21][1500];
+    char linhas[21]; //quantidade de linhas
+
+    for(i=0; i<21; i++){
+        for(j=0; j<21; j++){
+            arra[j] = '?';
+            for(i=0; i<21; i++){
+              linhas = '?';
+            }
+        }
+    }
+    if ( file != NULL ){
+        i=0;
+        while ( fgets ( linhas, sizeof linhas, file ) != NULL ){
+            strcpy(arra, linhas);
+            printf("array ----> %s ", &arra);
+            i++;
+        }
+        fclose ( file );
+    }else{
+        perror ( filename );
+    }
+    return 0;
+}
+
+//TESTE
+int lista(){
+    int num; // ESTE É O NUMERO QUE SERÁ LIDO DO ARQUIVO E PASSADO PARA A LISTA
+    tlista * list; // ESTOU CRIANDO A LISTA, NADA DE NOVO
+    list = NULL;
+    FILE *arquivo; // REFERENCIA DO ARQUIVO, SEMPRE QUE FOR UTILIZAR O ARQUIVO
+                   // SERA REFERENDIDO-SE PELO arquivo
+    arquivo = fopen("c:\\arq.txt","r"); // O FOPEN ABRE O ARQUIVO, NO CASO
+                                        // É PASSADO POR PARAMETRO O ENDEREÇO
+                                        // DO ARQUIVO, E O "r" SIGNIFICA QUE
+                                        // O ARQUIVO ESTÁ SENDO ABERTO APENAS
+                                        // PARA LEITURA
+
+    if (arquivo) // CASO NAO TENHA DADO ERRO AO ABRIR O ARQUIVO
+       {
+           do
+             {
+                fscanf(arquivo,"%d",&num ); // O FSCANF É IDENTICO AO SCANF QUE
+                                            // TODOS CONHECEM, PORÉM ELE É
+                                            // UTILIZADO PARA LER DADOS DE
+                                            // ARQUIVOS, E É PASSADO COMO
+                                            // PARAMETRO, O PROPRIO ARQUIVO
+                                            // QUE ESTA SENDO LIDO
+                list = lu_insereinicio(list, num); // INSERE O NUMERO NA LISTA
+             }while(!feof(arquivo)); // ELE PERCORRERÁ TODAS AS LINHAS DO ARQUIVO
+                                     // ENQUANTO O ARQUIVO NAO CHEGAR AO FIM
+                                     // FEOF = END OF FILE
+         }
+
+    lu_exibelista(list); // EXIBE A LISTA
+    fclose(arquivo); // FECHA O ARQUIVO
+}
 
 #endif // PERGUNTAS_H_INCLUDED
