2b3c8f5a2773b3ce8adf6a73369f7915005ff6d4
realvesd <43302742+realvesd@users.noreply.github.com>
2019-05-20 15:20:38 -0300

    Create about.c

---
+++ Cositas/about.c
@@ -0,0 +1,12 @@
+Esse é um quiz elaborado para testar seus conhecimentos gerais sobre as mais diversas áreas. Ele é dividido em três categorias de dificuldade: Fácil, Intermediário e Difícil. Cada nível tem 10 perguntas pra te desafiar. Você consegue passar por todos os níveis?
+A pontuação é dada da seguinte forma: Cada acerto no nível fácil equivale a 10 pontos, no Intermediário 20 pontos e no Difícil 30 pontos. Caso você acerte menos de 5 perguntas, o jogo dará GAME OVER. Senão, sua pontuação segue pro próximo nível e será acumulada com os novos acertos.
+Está pronto pra jogar? S/N
+Escolha um nível: F, I, D
+
+Parabéns, você completou o nível fácil! Está indo muito bem, deseja tentar o nível intermediário?
+Oh, não foi dessa vez, precisa praticar mais um pouco! Deseja jogar novamente?
+
+Parabéns, você completou o nível intermediário! Seu nível de conhecimento é acima da média, deseja tentar o nível difícil?
+Oh, não foi dessa vez, precisa praticar mais um pouco! Deseja jogar novamente?
+
+Parabéns, você completou o nível difícil!, deseja tentar o nível difícil?
